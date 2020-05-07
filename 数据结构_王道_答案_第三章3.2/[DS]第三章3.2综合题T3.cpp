809ae571cfb6c88c660d28dc73d87fb15f36f64b
yuxiaoyang88 <yuxiaoyang88@sina.com>
2020-05-07 13:34:26 +0800

    提出一个逻辑上bug并解决

--- 数据结构_王道_答案_第三章3.2/[DS]第三章3.2综合题T3.cpp
+++ 数据结构_王道_答案_第三章3.2/[DS]第三章3.2综合题T3.cpp
@@ -13,16 +13,20 @@ int main(int argc, char** argv) {
     //初始化
     SqStack<int> stackA;
     SqStack<int> stackB;
+    //入队
     for (int i = 0; i < 10; i++) {
         EnQueue(stackA, stackB, i);
     }
+    //打印
     stackA.print();
     stackB.print();
     int x;
+    //出队
     for (int i = 0; i < 10; i++) {
         DeQueue(stackA, stackB, x);
         cout << "拿出的元素为:" << x << endl;
     }
+    //打印
     stackA.print();
     stackB.print();
     return 1;
@@ -30,8 +34,20 @@ int main(int argc, char** argv) {
 //入队
 template<class ElemType>
 bool EnQueue(SqStack<ElemType>& stackA, SqStack<ElemType>& stackB, int x) {
-    //如果A栈满了，但B栈满了，不做任何处理
-    if (stackA.isFull() && stackB.isFull()) {
+    // 先前我使用的判定条件为 
+    // 如果A栈满了，但B栈满了，不做任何处理
+    // if (stackA.isFull() && stackB.isFull()) {}
+    // 我想到了一种情况，能够导致bug
+    // 答案中我默认使用stackA和stackB的容量为5
+    // 队列压入9个元素，拿出2个元素，再压入1个元素
+    // 此时stackB非空未满，stackA满了，不会触发两个if条件
+    // 再次压入一个元素，stackA满了，肯定压不进去
+    // 会stackA.push(x);并return true;
+    // 这样是不对的，因而我把它改变成了下面这样
+    // 虽然会缩短队列容量，但不会引发错误
+    // stackA中元素必须一次性全部压入stackB
+    // 才能保证队列的性质
+    if (stackA.isFull() && !stackB.isEmpty()) {
         return false;
     }
     //如果A栈满了，B栈为空，把A栈所有元素拿到B栈中，再从B栈拿出一个元素
