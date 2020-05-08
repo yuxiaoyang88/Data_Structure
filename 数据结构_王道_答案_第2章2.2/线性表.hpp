3b7bb93e88b9f1f2c47ec52804727cee392d03c2
yuxiaoyang88 <yuxiaoyang88@sina.com>
2020-05-08 21:46:12 +0800

    美化

--- 数据结构_王道_答案_第2章2.2/线性表.hpp
+++ 数据结构_王道_答案_第2章2.2/线性表.hpp
@@ -1,8 +1,8 @@
+#ifndef PRINT_H
+#define PRINT_H
 #include <iostream>
-#include <cstring>
-#include <sstream>
-#include <vector>
-#include <math.h>
+#include "打印.hpp"
+#endif
 using namespace std;
 
 template<class ElemType>
@@ -25,44 +25,6 @@ void SqList<ElemType>::create(ElemType data[], int length) {
     this->length = length;
 }
 
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-//下面所有的代码都是为了友好的输出数组，不要再看了
-//调用方式
-// PrintData::print(数组, 数组长度);
-template<class ElemType>
-int getDigitCapacity(ElemType num);
-
-template<class ElemType>
-class PrintData {
-public:
-    int      index;
-    ElemType data;
-    // indexLength - dataLength
-    int minus;
-
-public:
-    //构造函数
-    PrintData(int index, ElemType data) : index(index), data(data) {
-        minus = getDigitCapacity(index) - getDigitCapacity(data);
-    }
-};
-
-//这个方法不能放在类中
-//否则ElemType会随着类的ElemType而变化
-//会影响构造函数中getDigitCapacity(index)的类型
-template<class ElemType>
-int getDigitCapacity(ElemType num) {
-    //用cout的格式把ElemType转为string
-    stringstream stream;
-    string       str;
-    stream << num;
-    stream >> str;
-    return str.length();
-}
-
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
 //打印
 template<class ElemType>
 void SqList<ElemType>::print() const {
