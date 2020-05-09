adbb92f2807c3fc4ab5dce0c8b03c7aa45440b7e
yuxiaoyang88 <yuxiaoyang88@sina.com>
2020-05-09 16:30:08 +0800

    修复：将[DS]第三章3.1综合题T5.cpp模板化

--- 数据结构_王道_答案_第3章3.1/[DS]第三章3.1综合题T5.cpp
+++ 数据结构_王道_答案_第3章3.1/[DS]第三章3.1综合题T5.cpp
@@ -3,77 +3,81 @@ using namespace std;
 
 //共享存储区的顺序栈本身就是个数组
 // A从0向19方向存，B从19向0方向存
-struct DoubleStack {
-    int data[20];
-    int topA;
-    int topB;
+template<class ElemType>
+struct SqStack {
+    ElemType data[20];
+    int      topA;
+    int      topB;
+
+public:
+    // str是用来确定是存A还是B
+    bool push(ElemType& x, string str);
+    bool pop(ElemType& x, string str);
 };
 
-// str是用来确定是存A还是B
-bool DoubleStack_Push(DoubleStack& stack, int x, string str);
-bool DoubleStack_Pop(DoubleStack& stack, string str, int& x);
-
 int main(int argc, char** argv) {
-    DoubleStack* stack = (DoubleStack*)malloc(sizeof(DoubleStack));
-    stack->topA        = -1;
-    stack->topB        = 20;
+    SqStack<int> stack;
+    stack.topA = -1;
+    stack.topB = 20;
     for (int i = 0; i < 15; i++) {
-        DoubleStack_Push(*stack, i, "A");
+        stack.push(i, "A");
     }
     for (int i = 0; i < 10; i++) {
-        DoubleStack_Push(*stack, i, "B");
+        stack.push(i, "B");
     }
     int x;
-    DoubleStack_Pop(*stack, "A", x);
+    stack.pop(x, "A");
     cout << "拿出栈A的元素为：" << x << endl;
-    DoubleStack_Pop(*stack, "A", x);
+    stack.pop(x, "A");
     cout << "拿出栈A的元素为：" << x << endl;
-    DoubleStack_Pop(*stack, "A", x);
+    stack.pop(x, "A");
     cout << "拿出栈A的元素为：" << x << endl;
-    DoubleStack_Pop(*stack, "B", x);
+    stack.pop(x, "B");
     cout << "拿出栈B的元素为：" << x << endl;
-    DoubleStack_Pop(*stack, "B", x);
+    stack.pop(x, "B");
     cout << "拿出栈B的元素为：" << x << endl;
-    DoubleStack_Pop(*stack, "B", x);
+    stack.pop(x, "B");
     cout << "拿出栈B的元素为：" << x << endl;
     return 1;
 }
 
 // str是用来确定是存A还是B
-bool DoubleStack_Push(DoubleStack& stack, int x, string str) {
+template<class ElemType>
+bool SqStack<ElemType>::push(ElemType& x, string str) {
     //如果栈满就不能再向里面放元素了
     // A的栈顶后面就是B的栈顶
-    if (stack.topA + 1 >= stack.topB) {
+    if (this->topA + 1 >= this->topB) {
         return false;
     }
     if (str == "A") {
-        stack.topA++;
-        stack.data[stack.topA] = x;
+        this->topA++;
+        this->data[this->topA] = x;
     }
     if (str == "B") {
-        stack.topB++;
-        stack.data[stack.topB] = x;
+        this->topB++;
+        this->data[this->topB] = x;
     }
     return true;
 }
 
-bool DoubleStack_Pop(DoubleStack& stack, string str, int& x) {
+template<class ElemType>
+bool SqStack<ElemType>::pop(ElemType& x, string str) {
     //可以写成一个if，感觉更难理解一些
     // str == "A" && stack.topA == -1 || str == "B" && stack.topB == 20
-    if (str == "A" && stack.topA == -1) {
+    if (str == "A" && this->topA == -1) {
         return false;
     }
-    if (str == "B" && stack.topB == 20) {
+    if (str == "B" && this->topB == 20) {
         return false;
     }
     if (str == "A") {
-        x = stack.data[stack.topA];
-        stack.topA--;
+        x = this->data[this->topA];
+        this->topA--;
         return true;
     }
     if (str == "B") {
-        x = stack.data[stack.topB];
-        stack.topB++;
+        x = this->data[this->topB];
+        this->topB++;
         return true;
     }
     // warning: non-void function does not return a value in all control paths
