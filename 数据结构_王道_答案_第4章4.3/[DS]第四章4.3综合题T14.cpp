6fd719362d88b87f689fe7c2eceaaa562f7e16ce
yuxiaoyang88 <yuxiaoyang88@sina.com>
2020-05-11 22:33:10 +0800

    [DS]第四章4.3综合题T14.cpp

--- 数据结构_王道_答案_第4章4.3/[DS]第四章4.3综合题T14.cpp
+++ 数据结构_王道_答案_第4章4.3/[DS]第四章4.3综合题T14.cpp
@@ -3,35 +3,39 @@
 #include <map>
 #include "二叉排序树.hpp"
 using namespace std;
-void BST_xxx(BiTree& tree);
 
+template<class Elemtype>
+void BST_xxx(BitNode<Elemtype>& tree);
+
+template<class ElemType>
 struct LevelNode {
-    BitNode* bitnode;
+    BitNode<ElemType>* bitnode;
     int      level;
 };
 
 int main(int argc, char** argv) {
     //初始化
-    BiTree tree   = NULL;
-    int    a[]    = {40, 72, 38, 35, 67, 51, 90, 8, 55, 21};
-    int    length = sizeof(a) / sizeof(a[0]);
-    BST_Insert(tree, a, length);
+    BitNode<int> tree;
+    const int    length       = 10;
+    int          data[length] = {40, 72, 38, 35, 67, 51, 90, 8, 55, 21};
+    tree.create(data, length);
     //遍历
-    BST_PreOrderFormat(tree);
-    BST_InOrderFormat(tree);
-    BST_PostOrderFormat(tree);
+    tree.printPre();
+    tree.printIn();
+    tree.printPost();
     //使用
-    queue<LevelNode*> queue;
+    queue<LevelNode<int>*> queue;
     BST_xxx(tree);
     return 1;
 }
 
-void BST_xxx(BiTree& tree) {
+template<class Elemtype>
+void BST_xxx(BitNode<Elemtype>& tree) {
     //队列用来层次遍历并记录高度
-    LevelNode* node = (LevelNode*)malloc(sizeof(LevelNode));
-    node->bitnode   = tree;
+    LevelNode<Elemtype>* node = (LevelNode<Elemtype>*)malloc(sizeof(LevelNode<Elemtype>));
+    node->bitnode   = &tree;
     node->level     = 1;
-    queue<LevelNode*> queue;
+    queue<LevelNode<Elemtype>*> queue;
     queue.push(node);
     // Map用来计算哪一层元素最多
     map<int, int> map;
@@ -49,13 +53,13 @@ void BST_xxx(BiTree& tree) {
         }
         cout << "第" << node->level << "层的" << node->bitnode->data << endl;
         if (node->bitnode->left_child != NULL) {
-            LevelNode* newnode = (LevelNode*)malloc(sizeof(LevelNode));
+            LevelNode<Elemtype>* newnode = (LevelNode<Elemtype>*)malloc(sizeof(LevelNode<Elemtype>));
             newnode->bitnode   = node->bitnode->left_child;
             newnode->level     = node->level + 1;
             queue.push(newnode);
         }
         if (node->bitnode->right_child != NULL) {
-            LevelNode* newnode = (LevelNode*)malloc(sizeof(LevelNode));
+            LevelNode<Elemtype>* newnode = (LevelNode<Elemtype>*)malloc(sizeof(LevelNode<Elemtype>));
             newnode->bitnode   = node->bitnode->right_child;
             newnode->level     = node->level + 1;
             queue.push(newnode);
