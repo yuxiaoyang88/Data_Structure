c441e57ddc14d1fa41b5bc8be36c3567f68dec15
yuxiaoyang88 <yuxiaoyang88@sina.com>
2020-05-11 22:19:20 +0800

    修复：[DS]第四章4.3综合题T12.cpp

--- 数据结构_王道_答案_第4章4.3/[DS]第四章4.3综合题T12.cpp
+++ 数据结构_王道_答案_第4章4.3/[DS]第四章4.3综合题T12.cpp
@@ -2,9 +2,9 @@
 #include "顺序栈.hpp"
 #include "二叉排序树.hpp"
 using namespace std;
-// bool BST_PrintRoots(BiTree& root, int x);
 
-// SqStack sqstack;
+template<class ElemType>
+bool BST_PrintRoots(BitNode<ElemType>* node, int x, SqStack<ElemType>& stack);
 
 int main(int argc, char** argv) {
     //初始化
@@ -16,31 +16,32 @@ int main(int argc, char** argv) {
     tree.printPre();
     tree.printIn();
     tree.printPost();
-    // //因为BST_PrintRoots使用栈保存元素
-    // //因为方法本身是递归的，所以初始化栈的方法要放在外面
-    // //所以先初始化一下栈
-    // //最后再打印一下栈
-    // SqStack_Init(sqstack);
-    // BST_PrintRoots(tree, 51);
-    // SqStack_Show(sqstack);
+    //因为BST_PrintRoots使用栈保存元素
+    //因为方法本身是递归的，所以初始化栈的方法要放在外面
+    //所以先初始化一下栈
+    //最后再打印一下栈
+    SqStack<int> stack;
+    BST_PrintRoots(&tree, 51, stack);
+    stack.print();
     return 1;
 }
 
-// bool BST_PrintRoots(BiTree& root, int x) {
-//     if (root == NULL) {
-//         return false;
-//     }
-//     if (root->data == x) {
-//         return true;
-//     }
-//     if (BST_PrintRoots(root->left_child, x) ||
-//         BST_PrintRoots(root->right_child, x)) {
-//         //这样打印出来是逆序的
-//         //因为递归到最小结点才会打印
-//         //所以压入栈，搞成顺序的
-//         // cout << root->data << endl;
-//         SqStack_Push(sqstack, root->data);
-//         return true;
-//     }
-//     return false;
-// }
+template<class ElemType>
+bool BST_PrintRoots(BitNode<ElemType>* node, int x, SqStack<ElemType>& stack) {
+    if (!node) {
+        return false;
+    }
+    if (node->data == x) {
+        return true;
+    }
+    if (BST_PrintRoots(node->left_child, x, stack) ||
+        BST_PrintRoots(node->right_child, x, stack)) {
+        //这样打印出来是逆序的
+        //因为递归到最小结点才会打印
+        //所以压入栈，搞成顺序的
+        // cout << root->data << endl;
+        stack.push(node->data);
+        return true;
+    }
+    return false;
+}
